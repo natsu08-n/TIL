3d03ee3e53035886b25a5a5c8d93d06a0698140c
natsu08-n <merrysheep18@gmail.com>
2023-10-31 09:44:33 +0900

    3.7.5　プリミティブなのにプロパティがある？

--- TypeScript/blueberry_book/src/index.ts
+++ TypeScript/blueberry_book/src/index.ts
@@ -1,2 +1,13 @@
-const message:string="Hello,world";
-console.log(message);
+// 3.7.5　プリミティブなのにプロパティがある？
+// type HasLength = {length: number};
+// const obj: HasLength = "foober";
+// console.log(obj);
+//プリミティブであるstringがオブジェクト型に適合する
+//実はプリミティブに対してプロパティアクセスを行うたびに一時的にオブジェクトが作成されるから。プロパティアクセスが終了したらこのオブジェクトも消える。
+
+let val: {} = 123;
+// val = "foober";
+// val = {num: 1234};
+// val = null; //errorになる
+// val = undefined; //errorになる
+console.log(val);
